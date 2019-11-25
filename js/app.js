1967f5e7a52aac18eab2955b58c2d5e25237366a
jfdignard <jeff.dignard@outlook.com>
2019-11-25 13:17:40 -0500

    Ajout d'une fonction d'affichage app.js/Ajout regle .gitignore/Ajout de source index.html

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 // Ceci est le main JS de mon web app
-        $(document).ready(function() {
-            $("div").addClass("hide").fadeIn(2000).removeClass("hide");
-        });
+
+    $(document).ready(function() {
+        $("div").addClass("hide").fadeIn(2000).removeClass("hide");
+        
+    });
+
+// Ajout d'une nouvelle fonction et correction de concaténation (jfdignard)
+
+function hidingClass() {
+    $("div").addClass("hide").fadeIn(2000).removeClass("hide");
+};
